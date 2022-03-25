f83e1d2d7bbe4cead34811e712618fe964a45726
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-03-25 15:39:10 -0600

    coorrected the incidents by rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'support/factory_bot'
 require 'support/database_cleaner'
 require 'support/shoulda_matchers'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
