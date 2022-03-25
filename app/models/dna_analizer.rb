8933cc22f624b38a8d41bf2705dfd6e23a2b28ca
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-03-25 15:00:37 -0600

    add routes

--- app/models/dna_analizer.rb
+++ app/models/dna_analizer.rb
@@ -12,9 +12,13 @@ class DnaAnalizer < ApplicationRecord
   validates :dna, presence: true
   validate :is_array?
 
+  after_save :is_mutant?
+
   def is_array?
     !!JSON.parse(dna)
   rescue JSON::ParserError
     errors.add(:dna, "isn't a array")
   end
+
+  def is_mutant?; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/mutant', to: 'dna_analizers#mutant'
+      get '/stat', to: 'dna_analizers#stat'
     end
   end
   mount Rswag::Ui::Engine => '/api-docs'
