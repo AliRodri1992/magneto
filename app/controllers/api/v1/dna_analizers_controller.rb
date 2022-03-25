d6a2b71cec39f19c6cb47efadcae0339db969516
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-03-25 15:38:12 -0600

    initialize or find

--- app/controllers/api/v1/dna_analizers_controller.rb
+++ app/controllers/api/v1/dna_analizers_controller.rb
@@ -1,12 +1,19 @@
 class Api::V1::DnaAnalizersController < ApplicationController
+  before_action :initialize_or_find, only: %i[mutant]
   def mutant
-    dna_analizer = DnaAnalizer.new(dna: params[:dna])
-    if dna_analizer.save
-      render json: dna_analizer, status: :ok, code: '200'
+    if @dna_analizer.save
+      render json: @dna_analizer, status: :ok, code: '200'
     else
-      render json: dna_analizer.errors, status: :unprocessable_entity, code: '422'
+      render json: @dna_analizer.errors, status: :unprocessable_entity, code: '422'
     end
   end
 
   def stat; end
+
+  private
+
+  def initialize_or_find
+    @dna_analizer = DnaAnalizer.find_by(dna: params[:dna])
+    @dna_analizer = DnaAnalizer.new(dna: params[:dna]) if @dna_analizer.nil?
+  end
 end
