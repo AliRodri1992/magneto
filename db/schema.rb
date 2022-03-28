9f8c912a15bdde122144b9d247a2062d4723ffd1
Ali Rodriguez <ali.rodri.vasquez@gmail.com>
2022-03-28 05:46:15 -0600

    model user

--- db/schema.rb
+++ db/schema.rb
@@ -22,4 +22,11 @@ ActiveRecord::Schema.define(version: 2022_03_25_183132) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
